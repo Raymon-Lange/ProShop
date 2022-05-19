b77d89bca46b8c32ca019993f64609392f271cbc
Raymon Lange <raymon.lange@gmail.com>
2022-05-19 16:31:31 -0500

    Added Headers and Footer and Main page

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+import { Container, Nav, Navbar,NavDropdown } from 'react-bootstrap'
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
+              <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
