8b2eece0f85c251773516098c8e163467f293580
Raymon Lange <raymon.lange@gmail.com>
2022-05-21 18:34:16 -0500

    Added a product component

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -15,9 +15,9 @@ const products = [
   {
     _id: '2',
     name: 'Big Bertha B21 Drivers',
-    image: '/images/BigBertha.jpg',
+    image: '/images/BigBertha.png',
     description:
-      'You could be hitting the ball farther, and that\’s why we\’re bringing out the new Big Bertha B21. It\’s a new formula for distance, built to take the big miss out of your game and invite you back to the fairway.',
+      'You could be hitting the ball farther, and that’s why we\’re bringing out the new Big Bertha B21. It\’s a new formula for distance, built to take the big miss out of your game and invite you back to the fairway.',
     brand: 'Callaway',
     category: 'Driver',
     price: 349.99,
@@ -28,7 +28,7 @@ const products = [
   {
     _id: '3',
     name: 'STEALTH',
-    image: '/images/stealth.jpg',
+    image: '/images/Steath.jpg',
     description:
       'The future of driver performance begins with our 60x Carbon Twist Face comprised of 60 layers of carbon sheets strategically arranged for better energy transfer and faster ball speeds across a large area of the face.',
     brand: 'Taylor Made',
@@ -41,7 +41,7 @@ const products = [
   {
     _id: '4',
     name: 'TP5 GOLF BALLS',
-    image: '/images/tp5.jpg',
+    image: '/images/TP5.jpg',
     description:
       'Every shot in golf is different. Some are easy. Others, not so much. But with TP5/TP5x and its better aerodynamics, improved green side spin and enhanced feel, you have a ball that gives you the confidence to take on any situation. So, no matter what the course throws at you, the complete performance of TP5/TP5x lets you love every shot.',
     brand: 'Taylor Made',
@@ -54,7 +54,7 @@ const products = [
   {
     _id: '5',
     name: 'TOUR RESPONSE',
-    image: '/images/mouse.jpg',
+    image: '/images/tourResponse.jpg',
     description:
       'A Tour-quality golf ball with a 100% cast urethane cover, designed for high performance at a lower price. It\’s soft feel and more spin, with less spend.',
     brand: 'Taylor Made',
@@ -67,7 +67,7 @@ const products = [
   {
     _id: '6',
     name: 'Chrome Soft',
-    image: '/images/chrome.pny',
+    image: '/images/chrome.png',
     description:
       'Chrome Soft is better for everyone, from amateurs to major winners. Now with our proprietary new Precision Technology, the Chrome Soft Golf Ball is longer through the bag while providing the signature soft feel that golfers love.',
     brand: 'Callaway',
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {Row, Col} from 'react-bootstrap'
+import Product from '../components/Product'
 import products from '../products'
 
 function HomeScreen() {
   return (
     <>
-        <h1>Lastest products</h1>
+        <h1>Lastest products!</h1>
         <Row>
             {products.map((product) =>(
                 <Col sm={12} md={6} lg={4}>
-                    <h3>{product.name}</h3>
+                    <Product product={product} />
                 </Col>
                 ))}
         </Row>
