d6b31716feed4238df7935f1c7e0fc6d12e47e6a
Raymon Lange <raymon.lange@gmail.com>
2022-05-23 14:11:21 -0500

    added the start of the backend by creating a express app for serving products.js

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Rogue ST MAX',
+    image: '/images/Rogue.png',
+    description:
+      'Rogue ST MAX is our highest MOI head with a slight draw bias that\'s built for all levels of golfers. This is the best combination of distance and forgiveness in a Callaway driver.',
+    brand: 'Callaway',
+    category: 'Driver',
+    price: 549.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Big Bertha B21 Drivers',
+    image: '/images/BigBertha.png',
+    description:
+      'You could be hitting the ball farther, and that’s why we’re bringing out the new Big Bertha B21. It’s a new formula for distance, built to take the big miss out of your game and invite you back to the fairway.',
+    brand: 'Callaway',
+    category: 'Driver',
+    price: 349.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'STEALTH',
+    image: '/images/Steath.jpg',
+    description:
+      'The future of driver performance begins with our 60x Carbon Twist Face comprised of 60 layers of carbon sheets strategically arranged for better energy transfer and faster ball speeds across a large area of the face.',
+    brand: 'Taylor Made',
+    category: 'Driver',
+    price: 579.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'TP5 GOLF BALLS',
+    image: '/images/TP5.jpg',
+    description:
+      'Every shot in golf is different. Some are easy. Others, not so much. But with TP5/TP5x and its better aerodynamics, improved green side spin and enhanced feel, you have a ball that gives you the confidence to take on any situation. So, no matter what the course throws at you, the complete performance of TP5/TP5x lets you love every shot.',
+    brand: 'Taylor Made',
+    category: 'Balls',
+    price: 49.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'TOUR RESPONSE',
+    image: '/images/tourResponse.jpg',
+    description:
+      'A Tour-quality golf ball with a 100% cast urethane cover, designed for high performance at a lower price. It’s soft feel and more spin, with less spend.',
+    brand: 'Taylor Made',
+    category: 'Balls',
+    price: 39.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Chrome Soft',
+    image: '/images/chrome.png',
+    description:
+      'Chrome Soft is better for everyone, from amateurs to major winners. Now with our proprietary new Precision Technology, the Chrome Soft Golf Ball is longer through the bag while providing the signature soft feel that golfers love.',
+    brand: 'Callaway',
+    category: 'Balls',
+    price: 49.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
