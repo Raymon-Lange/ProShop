fb163534fc369b14bc028ff2ba307bfcdc777461
Raymon Lange <raymon.lange@gmail.com>
2022-05-23 08:47:38 -0500

    Added Rating components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,10 +6,10 @@ import products from '../products'
 function HomeScreen() {
   return (
     <>
-        <h1>Lastest products!</h1>
+        <h1>Lastest products</h1>
         <Row>
             {products.map((product) =>(
-                <Col sm={12} md={6} lg={4}>
+                <Col key={product._id} sm={12} md={6} lg={4}>
                     <Product product={product} />
                 </Col>
                 ))}
