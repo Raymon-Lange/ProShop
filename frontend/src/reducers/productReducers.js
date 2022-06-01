dca00b97f9bb022a849e4bfc4e5b6310167995a9
Raymon Lange <raymon.lange@gmail.com>
2022-06-01 08:30:33 -0500

    Updated Home page to pull products from the store

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,11 +1,11 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_SUCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants"
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants"
 
-export const productListReducer = (state = {proucts: []}, action ) => {
+export const productListReducer = (state = {products: []}, action ) => {
     switch(action.type){
         case PRODUCT_LIST_REQUEST:
             return {loading: true, products: [] }
         case PRODUCT_LIST_SUCESS:
-            return { loading: false, prodcuts: action.payload}
+            return {loading: false, products: action.payload}
         case PRODUCT_LIST_FAIL:
             return {loading: false, error: action.payload}
         default:
