c53cca654ef328e093135e5ea8dbd95172c4c0da
Raymon Lange <raymon.lange@gmail.com>
2022-06-01 10:35:34 -0500

    Now pulling product details from store updated action and reducers

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants"
+import { PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCESS, PRODUCT_DETAILS_FAIL } from "../constants/productConstants"
 
 export const listProducts = () => async (dispatch) => {
     try {
@@ -10,6 +11,19 @@ export const listProducts = () => async (dispatch) => {
         dispatch({type: PRODUCT_LIST_SUCESS, payload: data})
     } catch (error) {
         dispatch({type: PRODUCT_LIST_FAIL, payload : error.response && error.response.data.message 
+            ? error.response.data.message : error.message,})
+    }
+}
+
+export const listProductDetails = (productId) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST})
+
+        const {data} = await axios.get(`/api/products/${productId}`)
+
+        dispatch({type: PRODUCT_DETAILS_SUCESS, payload: data})
+    } catch (error) {
+        dispatch({type: PRODUCT_DETAILS_FAIL, payload : error.response && error.response.data.message 
             ? error.response.data.message : error.message,})
     }
 }
