c7971e5b61541200f8f7e0919344526e04fec865
Raymon Lange <raymon.lange@gmail.com>
2022-06-14 15:07:42 -0500

    Added the ablity to register a account

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,10 +1,12 @@
 import axios from "axios"
 import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from "../constants/usersContants"
+import { USER_REG_FAIL, USER_REG_REQUEST, USER_REG_SUCCESS } from "../constants/usersContants"
+
 
 export const login = (email, password) => async (dispatch ) => {
     try {
         dispatch({
-            type: USER_LOGIN_REQUEST
+            type: USER_LOGIN_REQUEST,
         })
 
         const config = {
@@ -30,4 +32,29 @@ export const login = (email, password) => async (dispatch ) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({type: USER_LOGOUT,})
+}
+
+export const register = (name, email, password) => async (dispatch ) =>  {
+    try {
+        dispatch({
+            type: USER_REG_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        }
+        const {data} = await axios.post('/api/users', {name, email, password}, config)
+
+        dispatch({
+            type: USER_REG_SUCCESS, 
+            payload: data
+        })
+
+    } catch (error){
+        dispatch({type: USER_REG_FAIL, payload : error.response && error.response.data.message 
+            ? error.response.data.message : error.message,})
+    }
+
 }
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,4 +1,5 @@
 import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from '../constants/usersContants'
+import { USER_REG_FAIL, USER_REG_REQUEST, USER_REG_SUCCESS } from "../constants/usersContants"
 
 export const userLoginReducer = (state = { products: [] }, action) => {
     switch (action.type) {
@@ -7,6 +8,21 @@ export const userLoginReducer = (state = { products: [] }, action) => {
         case USER_LOGIN_SUCCESS:
             return { loading: false, userInfo: action.payload }
         case USER_LOGIN_FAIL:
+            return { loading: false, error: action.payload }
+        case USER_LOGOUT:
+            return {}
+        default:
+            return state
+    }
+}
+
+export const userRegisterReducer = (state = { products: [] }, action) => {
+    switch (action.type) {
+        case USER_REG_REQUEST:
+            return { loading: true}
+        case USER_REG_SUCCESS:
+            return { loading: false, userInfo: action.payload }
+        case USER_REG_FAIL:
             return { loading: false, error: action.payload }
         case USER_LOGOUT:
             return {}
